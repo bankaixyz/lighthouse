577954111ac8526859534977960addeb5827e30b
petscheit <etscheitp@gmail.com>
2025-03-11 17:27:04 +0100

    feat: make get beacon leafs public

--- consensus/types/src/beacon_state.rs
+++ consensus/types/src/beacon_state.rs
@@ -2572,7 +2572,7 @@ impl<E: EthSpec> BeaconState<E> {
         Ok(proof)
     }
 
-    fn get_beacon_state_leaves(&self) -> Vec<Hash256> {
+    pub fn get_beacon_state_leaves(&self) -> Vec<Hash256> {
         let mut leaves = vec![];
         #[allow(clippy::arithmetic_side_effects)]
         match self {
