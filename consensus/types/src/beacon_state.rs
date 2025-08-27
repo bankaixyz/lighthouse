50561da35eb6f18d884a1a45d861ed40ce5d056a
petscheit <etscheitp@gmail.com>
2025-08-27 16:45:50 +0200

    chore: make get_proof pub

--- consensus/types/src/beacon_state.rs
+++ consensus/types/src/beacon_state.rs
@@ -2760,7 +2760,7 @@ impl<E: EthSpec> BeaconState<E> {
         Ok(proof)
     }
 
-    fn generate_proof(
+    pub fn generate_proof(
         &self,
         field_index: usize,
         leaves: &[Hash256],
@@ -2775,7 +2775,7 @@ impl<E: EthSpec> BeaconState<E> {
         Ok(proof)
     }
 
-    fn get_beacon_state_leaves(&self) -> Vec<Hash256> {
+    pub fn get_beacon_state_leaves(&self) -> Vec<Hash256> {
         let mut leaves = vec![];
         #[allow(clippy::arithmetic_side_effects)]
         match self {
